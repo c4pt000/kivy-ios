85201e26ace043dcf9f2712e62331f1b9d85d12c
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-11-21 17:50:40 -0500

    Update __init__.py

--- kivy_ios/recipes/freetype/__init__.py
+++ kivy_ios/recipes/freetype/__init__.py
@@ -4,7 +4,7 @@ import sh
 
 
 class FreetypeRecipe(Recipe):
-    version = "2.5.5"
+    version = "2.10.0"
     url = "https://download.savannah.gnu.org/releases/freetype/freetype-old/freetype-{version}.tar.bz2"
     library = "objs/.libs/libfreetype.a"
     include_dir = ["include", ("builds/unix/ftconfig.h", "config/ftconfig.h")]
